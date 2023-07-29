37ea8b67b262d3d1665e09908349a0c6669ac6c4
navpreetk1 <navpreet.k.brudite@gmail.com>
2023-07-30 02:12:21 +0530

    delete extra pythontask3

--- python_task3/Q6.py
+++
@@ -1,9 +0,0 @@
-#Question 6------------------------------------------------
-
-list=[(1,2),(2,4),(4,5),(4,2),(1,2)]
-set=set()
-for x in list:
-    if x is not set:
-       set.add(tuple(sorted(x)))
-
-print(set)
