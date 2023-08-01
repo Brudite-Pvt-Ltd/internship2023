df07c8dd72e47c4c20ee5a063d446e5d68e1dc3d
ankii16 <Ankita.p.brudite@gmail.com>
2023-08-01 15:35:42 +0530

    updated

--- python_task_5/dict_task_set_2/10.py
+++ python_task_5/dict_task_set_2/10.py
@@ -1,1 +1,10 @@
 #10.Write a function that takes a dictionary and returns a new dictionary containing only the key-value pairs where the key is a palindrome.
+def palindrome_dict(d):
+    new_dict = {}
+    for key, value in d.items():
+        if key.lower() == key[::-1].lower():
+            new_dict[key] = value
+    return new_dict
+
+d = {'navan' : 1 , 'pop' : 2 , 'preet' : 3}
+print(palindrome_dict(d))
