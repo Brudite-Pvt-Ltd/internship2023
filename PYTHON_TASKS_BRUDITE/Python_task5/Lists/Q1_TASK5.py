48c18808afdc22a91b0fd15526d7b21d5ab7ba33
FaisalBrudite <faisal.n.brudite@gmail.com>
2023-08-01 22:51:28 +0530

    Python

---
+++ PYTHON_TASKS_BRUDITE/Python_task5/Lists/Q1_TASK5.py
@@ -0,0 +1,6 @@
+# 1. Remove an element from a list by its value
+
+list1 = ["Green", "Yellow", "Blue"]
+print(list1)
+list1.pop(1)
+print(list1)
---
+++ PYTHON_TASKS_BRUDITE/Python_task5/Lists/Q6_TASK5.py
@@ -0,0 +1,3 @@
+# Q6. Create a list of all prime numbers upto a given number.
+
+    
