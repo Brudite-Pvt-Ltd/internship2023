a6917f409569bfd1aab74bd027daf179f63bf421
navpreetk1 <navpreet.k.brudite@gmail.com>
2023-08-02 12:37:24 +0530

    done with modifiation

--- python_tasks/python_BI_103_task5/dictionary/Q4.py
+++ python_tasks/python_BI_103_task5/dictionary/Q4.py
@@ -2,8 +2,8 @@ def key_greater_than_values(student,value):
     key_greater=dict()
     for x,y in student.items():
         if y>value:
-            key_greater=x
-            return key_greater
+           key_greater[x] = y
+    return key_greater
 
 student={'nav': 80,'laksh': 78,'harsh':82,'man':89}
 value=int(input("enter the value"))
--- python_tasks/python_BI_103_task5/list/Q5.py
+++ python_tasks/python_BI_103_task5/list/Q5.py
@@ -1,23 +1,7 @@
-# Split a list into evenly sized chunks.
 
-def split_fun(list1, n):
-    list2=list()
-    final_list=list()
+def split(list, n):
+    return [list[i:i + n] for i in range(0, len(list), n)]
 
-    for x in range(len(list1)):
-        if x == n:
-            break
-        else:
-            list2.append(list1[x])
-            list1.pop(x)
-
-    final_list.append(list2)
-    final_list.append(list1)
-    return final_list
-
-
-list1= [1,2,23,4,5,6,7,9]
-n = 4  
-new_list = split_fun(list1, n)
-
-print(new_list)
+list = [1,3,5,7,9,11]
+n = 3   
+print(split(list, n))
