1b73151d68b7be8675d8c0b0842c9eee59e526ab
FaisalBrudite <faisal.n.brudite@gmail.com>
2023-08-03 00:53:55 +0530

    Python

--- PYTHON_TASKS_BRUDITE/Python_task5/Lists/Q1_TASK5.py
+++ PYTHON_TASKS_BRUDITE/Python_task5/Lists/Q1_TASK5.py
@@ -1,6 +1,6 @@
 # 1. Remove an element from a list by its value
 
-list1 = ["Green", "Yellow", "Blue"]
-print(list1)
-list1.pop(1)
-print(list1)
+def ele_removed(list, val):
+    if val in list:
+        list.remove(val)
+    return list
