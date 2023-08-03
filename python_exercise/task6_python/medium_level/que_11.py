a7132b0b68f7f3f8444760b64b9880ba36a56b49
lakshita <lakshita.s.brudite@gmail.com>
2023-08-03 23:07:59 +0530

    task 6 medium level done

---
+++ python_exercise/task6_python/medium_level/que_11.py
@@ -0,0 +1,20 @@
+'''We are making n stone piles! The first pile has n stones. If n is even,
+then all piles have an even number of stones. If n is odd, all piles have an odd number of stones.
+Each pile must have more stones than the previous pile but as few as possible.
+Write a Python program to find the number of stones in each pile.
+
+'''
+def find_stones_in_piles(n):
+    piles = []
+    
+    if n % 2  == 0:
+        for i in range(1, n+1 , 2):
+            piles.append(i)
+    else:
+        for i in range(1, n, 2):
+            piles.append(i)
+    return piles
+
+n = 7
+stone_piles = find_stones_in_piles(n)
+print("Stones in a single pile =", stone_piles)
