7476c785f6a7d694fcae51757e7b45e1027e6a8d
VipulJ8441 <139967842+VipulJ8441@users.noreply.github.com>
2023-08-18 10:30:10 +0530

    Update que6.py

--- Python_task/PythonAssiment/Medium/que6.py
+++ Python_task/PythonAssiment/Medium/que6.py
@@ -5,10 +5,11 @@ Write a Python program to check if a number is a power of two using recursion.
 """
 
 def ispower2(num):
-    if isinstance(num, float):
-        return False
-    if num ==1:
-        return True
+    if num < 2:
+        if num ==1:
+            return True
+        else:
+            False
     return ispower2(num/2)
     
 
